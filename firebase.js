5c89064b0d0582e3a82d8fed322805c14e66ef34
Robert Gregg <hi@robertgregg.co.uk>
2021-01-27 20:36:25 +0000

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDhhwNgPC_dWrnhIjUlxlhavhvhxa_jgOU",
+  authDomain: "linkedin-clone-rob.firebaseapp.com",
+  projectId: "linkedin-clone-rob",
+  storageBucket: "linkedin-clone-rob.appspot.com",
+  messagingSenderId: "168108481703",
+  appId: "1:168108481703:web:4a2968fbb6388bc8d88f82",
+};
